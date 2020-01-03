65e79a290a97c27bcb3cfdf600bd32811273d96c
Niels Albarran <niels.albarran@limango.de>
2020-01-03 12:43:09 +0100

    a minimal go project with a useless test (that passes)

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cwd-nial/golang-docker-gitlab
+
+go 1.13
+
+require github.com/stretchr/testify v1.4.0
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestService_Main(t *testing.T) {
+	main()
+	assert.True(t, true)
+}
