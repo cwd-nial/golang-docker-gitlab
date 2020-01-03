1d6060ecb79ec7f9b3ccc80ad0a82b388d9d78af
Niels Albarran <niels.albarran@limango.de>
2020-01-03 13:01:28 +0100

    add a router, server and a docker-compose file to run the go application inside of it

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Go Ducks!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Go Ducks!"))
+	})
+
+	log.Println("Starting HTTP services at 8080")
+	err := http.ListenAndServe(":8080", r) // Goroutine will block here
+
+	if err != nil {
+		log.Println("An error occurred starting HTTP listener at port 8080")
+		log.Println("Error: " + err.Error())
+	}
 }
